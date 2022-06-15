8efeb2ca8349e951c49e34a26c250731fd61909d
Sergei <sergei.ampilov@gmail.com>
2022-06-15 09:53:19 +0300

    refactor

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function ImagePopup(props) {
+function ImagePopup({isOpen, onClose, card}) {
     return (
-        <div className={`popup popup_prefix_image images-full ${props.isOpen ? 'popup_visible' : ''}`}>            
+        <div className={`popup popup_prefix_image images-full ${isOpen ? 'popup_visible' : ''}`}>            
             <div className="images-full__content">
-                <button className="popup__close images-full__close" type="button" title="Закрыть" onClick={props.onClose}></button>
+                <button className="popup__close images-full__close" type="button" title="Закрыть" onClick={onClose}></button>
                 <img
-                    src={props.card.link}
-                    alt={props.card.name}
+                    src={card.link}
+                    alt={card.name}
                     className="images-full__img" />
-                <p className="images-full__text">{props.card.name}</p>
+                <p className="images-full__text">{card.name}</p>
             </div>
         </div>
     );

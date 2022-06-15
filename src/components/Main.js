58f2da6b90d148e56422e2ad920f7dc3cc683ca8
Sergei <sergei.ampilov@gmail.com>
2022-06-15 10:29:34 +0300

    refactor

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Card from './Card'
 import api from '../utils/Api.js'
 
-function Main(props) {
+function Main({ onEditAvatar, onEditProfile, onAddPlace, onCardClick }) {
 
     const [userName, setUserName] = React.useState('');
     const [userDescription, setUserDescription] = React.useState('');
     const [userAvatar, setUserAvatar] = React.useState('');
     const [cards, setCards] = React.useState([]);
+
+    const cardsElements = cards.map( el => <Card element={el} key={el._id} onCardClick={onCardClick}/> )
     
 
     React.useEffect(() => {
@@ -17,13 +19,15 @@ function Main(props) {
                 setUserName(profileData.name);
                 setUserDescription(profileData.about);
                 setUserAvatar(profileData.avatar);
-            });
+            })
+            .catch(err => console.log(`Ошибка.....: ${err}`));
 
         api.getCards()
             .then((cardList) => {
-                console.log(cardList);
                 setCards(cardList);
             })
+            .catch(err => console.log(`Ошибка.....: ${err}`));
+
     }, []);
 
 
@@ -33,19 +37,19 @@ function Main(props) {
                 <div className="profile__content">
                     <div className="profile__avatar-group">
                         <img src={userAvatar} alt="Логотип пользователя" className="profile__avatar" />
-                        <button title="Загрузить новый аватар" className="profile__avatar-button" onClick={props.onEditAvatar}></button>
+                        <button title="Загрузить новый аватар" className="profile__avatar-button" onClick={onEditAvatar}></button>
                     </div>
                     <div className="profile__info">
                         <h1 className="profile__title">{userName}</h1>
                         <p className="profile__subtitle">{userDescription}</p>
-                        <button className="profile__pen" type="button" title="Редактировать" onClick={props.onEditProfile}></button>
+                        <button className="profile__pen" type="button" title="Редактировать" onClick={onEditProfile}></button>
                     </div>
                 </div>
-                <button className="profile__add-button" type="button" title="Добавить" onClick={props.onAddPlace}></button>
+                <button className="profile__add-button" type="button" title="Добавить" onClick={onAddPlace}></button>
             </section>
             <section>
                 <ul className="elements">
-                    { cards.map( el => <Card element={el} key={el._id} onCardClick={props.onCardClick}/> ) }
+                    { cardsElements }
                 </ul>
             </section>
         </main>

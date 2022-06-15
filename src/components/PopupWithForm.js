de4f734c1b78b89334b101ab2820e772fc56fb78
Sergei <sergei.ampilov@gmail.com>
2022-06-15 09:33:53 +0300

    refactor

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function PopupWithForm(props) {
+function PopupWithForm({ name, isOpen, onClose, title, children}) {
 
     return (
         
-        <div className={`popup popup_prefix_${props.name} ${props.isOpen ? 'popup_visible' : ''}`}>
+        <div className={`popup popup_prefix_${name} ${isOpen ? 'popup_visible' : ''}`}>
             <div className="popup__container">
-                <button className="popup__close" type="button" title="Закрыть" onClick={props.onClose}></button>
-                <h3 className="popup__title">{props.title}</h3>
-                <form className='popup__form' name={`${props.name}`}>
-                    {props.children}
+                <button className="popup__close" type="button" title="Закрыть" onClick={onClose}></button>
+                <h3 className="popup__title">{title}</h3>
+                <form className='popup__form' name={`${name}`}>
+                    {children}
                     <button type="submit" className="popup__button-submit">Да</button>
                 </form>
             </div>

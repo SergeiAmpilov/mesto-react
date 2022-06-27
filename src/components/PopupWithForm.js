1c1ba25515ed1c08b360f06f8f3f6342e648137d
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-06-27 10:49:31 +0300

    fix name for form

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -8,7 +8,7 @@ function PopupWithForm({ name, isOpen, onClose, title, children, onSubmit}) {
             <div className="popup__container">
                 <button className="popup__close" type="button" title="Закрыть" onClick={onClose}></button>
                 <h3 className="popup__title">{title}</h3>
-                <form className='popup__form' name={`${name}`} onSubmit={onSubmit}>
+                <form className='popup__form' name={name} onSubmit={onSubmit}>
                     {children}
                     <button type="submit" className="popup__button-submit">Да</button>
                 </form>
